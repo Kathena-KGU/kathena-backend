38f6ba1cfc29edb3707a9bef67373e10fb6a0c6e
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-01-17 17:05:33 +0900

    feat: Backend Initial Setup

---
+++ src/main/groovy/com/kathena/kathenabackend/KathenaBackendApplication.groovy
@@ -0,0 +1,13 @@
+package com.kathena.kathenabackend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KathenaBackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(KathenaBackendApplication, args)
+    }
+
+}
