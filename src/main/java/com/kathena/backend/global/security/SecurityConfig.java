87ab3232eecc224b195eadac58f3548555c321a8
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-01-25 22:26:49 +0900

    feat: JwtTokenProvider 및 TokenDto 구현 및 토큰 파일 정리 (#8)

--- src/main/java/com/kathena/backend/global/security/SecurityConfig.java
+++ src/main/java/com/kathena/backend/global/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.kathena.backend.global.config;
+package com.kathena.backend.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
